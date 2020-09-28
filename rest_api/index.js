9be251b8f6ddfe54f114122192a7ee245a457324
Cris-Mur <yomy98@gmail.com>
2020-09-27 23:41:09 -0500

    [13 - Express Router - migrate the endpoints of index to routes/main and routes/passwords]

--- rest_api/index.js
+++ rest_api/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { request, response } = require('express');
+
+const routes = require('./routes/main');
+const passwordRoutes = require('./routes/password');
+
 const app = express();
 const port = 3000;
 
@@ -10,66 +14,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application json data
 app.use(bodyParser.json());
 
-app.get('/', (request, response) => {
-  console.log('I see you');
-  response.send('hola bebe');
-});
-
-app.get('/status', (request, response) => {
-  response.status(200).json({ message: 'invalid body', status: 200 });
-});
-
-app.post('/signup', (request, response, next) => {
-  if (!request.body) {
-    response.status(400).json({ message: 'invalid body', status: 400 });
-  } else {
-    response.status(200).json({ message: 'ok', status: 200 });
-  }
-});
-
-app.post('/login', (request, response) => {
-  if (!request.body) {
-    response.status(400).json({ message: 'invalid body', status: 400 });
-  } else {
-    response.status(200).json({ message: 'ok', status: 200 });
-  }
-});
-
-app.post('/logout', (request, response) => {
-  if (!request.body) {
-    response.status(400).json({ message: 'invalid body', status: 400 });
-  } else {
-    response.status(200).json({ message: 'ok', status: 200 });
-  }
-});
-
-app.post('/token', (request, response) => {
-  if (!request.body || !request.body.refreshToken) {
-    response.status(400).json({ message: 'invalid body', status: 400 });
-  } else {
-    const { refreshToken } = request.body;
-    response.status(200).json({ message: `refresh token requested for token: ${refreshToken}`, status: 200 });
-  }
-});
-
-app.post('/forgot-password', (request, response) => {
-  if (!request.body || !request.body.email) {
-    response.status(400).json({ message: 'invalid body', status: 400 });
-  } else {
-    const { email } = request.body;
-    response.status(200).json({ message: `forgot password requested for email: ${email}`, status: 200 });
-  }
-});
-
-app.post('/reset-password', (request, response) => {
-  if (!request.body || !request.body.email) {
-    response.status(400).json({ message: 'invalid body', status: 400 });
-  } else {
-    const { email } = request.body;
-    response.status(200).json({ message: `password reset requested for email: ${email}`, status: 200 });
-  }
-});
-
+// setup routes
+app.use('/', routes);
+app.use('/', passwordRoutes);
 // catch all other routes
 app.use((request, response) => {
   response.status(404).json({ message: '404 - Not Found', status: 404 });
